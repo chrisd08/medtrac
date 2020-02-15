e4a60b0bb45a57662bd2ec4c1a19a2ecdb093f36
chrisd08 <dalechristian@gmail.com>
2020-02-15 11:20:40 +0000

    test: fix web tests

--- packages/web/src/App.test.js
+++ packages/web/src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+describe("App", () => {
+  it("renders without crashing", async () => {
+    await act(async () => {
+      ReactDOM.render(
+        <MemoryRouter initialEntries={["/"]}>
+          <App />
+        </MemoryRouter>,
+        container
+      );
+    });
+  });
 });
