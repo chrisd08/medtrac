43ab22c9df64ce4df68c7fc9a2b7e7c2a44d7ed6
vitalic <info@vitalic.co.uk>
2020-02-18 00:52:53 +0000

    build: correct audience value

--- packages/web/src/config/config.ts
+++ packages/web/src/config/config.ts
@@ -9,7 +9,7 @@ const dev = {
 const prod = {
   AUTH0_DOMAIN: "dev-dye9-qv5.eu.auth0.com",
   AUTH0_CLIENTID: "RNJDKHjLMMqnQE7qZ5GiC1IkgBLK0Bge",
-  AUTH0_AUDIENCE: "https://insulution-web.herokuapp.com",
+  AUTH0_AUDIENCE: "http://localhost",
   CLIENT_URL: "https://insulution-web.herokuapp.com",
   SERVER_URL: "https://insolution-prod.herokuapp.com",
 };
